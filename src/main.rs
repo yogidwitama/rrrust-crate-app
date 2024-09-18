5a00bfe3ddc6b92e3e528c00e7efb5708446ab77
unknown <yogidwitama56@gmail.com>
2024-09-18 10:14:53 +0700

    add sub package

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use say_hello_jan::{say_hello, say_goodbye, say_goodbye_everyone, say_hello_to_everyone};
+
+fn main() {
+    hello::say_hello();
+    bye::say_bye();
+
+    let response= say_hello("Yogi");
+    println!("{}", response);
+
+    let responsesayhelloevery=say_goodbye_everyone();
+    println!("{}", responsesayhelloevery);
+
+    let responsebye=say_goodbye("Dwitama");
+    println!("{}", responsebye);
+
+    let responsebyeeveryone =say_goodbye_everyone();
+    println!("{}", responsebyeeveryone)
+}
+#[test]
+fn test_uuid() {
+    let id =uuid::Uuid::new_v4().to_string();
+    println!("{}", id);
+}
